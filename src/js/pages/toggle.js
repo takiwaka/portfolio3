48e7b27ecff9945c2a09cc7541f78df98e54c91d
kawakita <hakusou.kokuyou@gmail.com>
2021-06-20 07:14:48 +0900

    header作成中

--- src/js/pages/toggle.js
+++ src/js/pages/toggle.js
@@ -1,7 +1,10 @@
 import $ from "jquery";
 
 $(function () {
-  $(".jsc-toggle-button").on("click", function () {
-    $(".jsc-nav-wrap,.jsc-toggle-button").toggleClass("is-open");
+  $(".jsc-toggle-button-main").on("click", function () {
+    $(".jsc-nav-wrap-main,.jsc-toggle-button-main").toggleClass("is-open");
+  });
+  $(".jsc-toggle-button-sub").on("click", function () {
+    $(".jsc-nav-wrap-sub").toggleClass("is-open");
   });
 });
