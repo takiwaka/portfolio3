371620cda3c4aebe15a669c8cdf75cea0edda77e
kawakita <hakusou.kokuyou@gmail.com>
2021-06-21 05:20:27 +0900

    アコーディオン実装完了

--- src/js/pages/toggle.js
+++ src/js/pages/toggle.js
@@ -2,9 +2,12 @@ import $ from "jquery";
 
 $(function () {
   $(".jsc-toggle-button-main").on("click", function () {
-    $(".jsc-nav-wrap-main,.jsc-toggle-button-main").toggleClass("is-open");
+    $(".jsc-toggle-button-main").toggleClass("is-open");
+    $(".jsc-nav-wrap-main").slideToggle();
+    return false;
   });
   $(".jsc-toggle-button-sub").on("click", function () {
-    $(".jsc-nav-wrap-sub").toggleClass("is-open");
+    $(".jsc-nav-wrap-sub").slideToggle();
+    return false;
   });
 });
