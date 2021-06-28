0843c3bdbca9b5e5b227f954cb3f9c2abab3821f
kawakita <hakusou.kokuyou@gmail.com>
2021-06-29 07:13:33 +0900

    fontawesome導入中

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 import $ from 'jquery';
 import 'slick-carousel';
 import router from './modules/Router'
-import '../scss/app.scss';
 import {config, dom, library} from '@fortawesome/fontawesome-svg-core';
 import {faDog, faCat} from '@fortawesome/free-solid-svg-icons';
 import {faComments} from '@fortawesome/free-regular-svg-icons';
+import '../scss/app.scss';
 
 library.add(faDog,faComments,faCat);
 
