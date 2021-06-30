076946b51de67bc2ad071c7ceb624b18af687338
k.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-06-30 13:38:51 +0900

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,20 @@ import $ from 'jquery';
 import 'slick-carousel';
 import router from './modules/Router'
 import '@fortawesome/fontawesome-free/js/all'
-// import {config, dom, library} from '@fortawesome/fontawesome-svg-core';
-// import {faDog, faCat} from '@fortawesome/free-solid-svg-icons';
-// import {faComments} from '@fortawesome/free-regular-svg-icons';
+import {config, dom, library} from '@fortawesome/fontawesome-svg-core';
+import {faDog, faCat,faLongArrowAltRight} from '@fortawesome/free-solid-svg-icons';
 
-import fontawesome from '@fortawesome/fontawesome'
-import faSolid from '@fortawesome/fontawesome-free-solid'
-import faRegular from '@fortawesome/fontawesome-free-regular'
-import faBrands from '@fortawesome/fontawesome-free-brands'
+// import fontawesome from '@fortawesome/fontawesome'
+// import faSolid from '@fortawesome/fontawesome-free-solid'
+// import faRegular from '@fortawesome/fontawesome-free-regular'
+// import faBrands from '@fortawesome/fontawesome-free-brands'
 
-fontawesome.library.add(faSolid, faRegular, faBrands)
+// fontawesome.library.add(faSolid, faRegular, faBrands)
 
 
 
 import '../scss/app.scss';
 
-// library.add(faDog,faComments,faCat);
-// dom.watch();
-fontawesome.dom.i2svg()
+library.add(faDog,faCat,faLongArrowAltRight);
+dom.watch();
+// fontawesome.dom.i2svg()
