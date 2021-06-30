dc9f8eaf89ecd6a83ffc90c345a386b07d5798b8
k.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-06-30 11:30:41 +0900

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,18 @@ import '@fortawesome/fontawesome-free/js/all'
 // import {config, dom, library} from '@fortawesome/fontawesome-svg-core';
 // import {faDog, faCat} from '@fortawesome/free-solid-svg-icons';
 // import {faComments} from '@fortawesome/free-regular-svg-icons';
+
+import fontawesome from '@fortawesome/fontawesome'
+import faSolid from '@fortawesome/fontawesome-free-solid'
+import faRegular from '@fortawesome/fontawesome-free-regular'
+import faBrands from '@fortawesome/fontawesome-free-brands'
+
+fontawesome.library.add(faSolid, faRegular, faBrands)
+
+
+
 import '../scss/app.scss';
 
 // library.add(faDog,faComments,faCat);
 // dom.watch();
+fontawesome.dom.i2svg()
