94a255d16bf7488f0204369fcd4050f2b5234137
kawakita <hakusou.kokuyou@gmail.com>
2021-07-01 07:12:08 +0900

    SCSS内でfontawesome使うの諦め（css-loaderのurl設定を有効にしないといけないから） fontリンク修正

--- src/js/pages/footer.js
+++ src/js/pages/footer.js
@@ -1,14 +1,1 @@
 import $ from "jquery";
-
-$(function () {
-  $(window).on("scroll", function () {
-    $(".jsc-footerScroll").each(function () {
-      var top = $(this).offset().top,
-          position = top - $(window).height();
-
-      if ($(window).scrollTop() > position) {
-        $(this).addClass("is-active");
-      }
-    });
-  });
-});
