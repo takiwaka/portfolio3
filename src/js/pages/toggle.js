83f5bafc23a61c0d80d2f4c16ab0d0fdf5401bec
k.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-07-14 17:40:56 +0900

    Update toggle.js

--- src/js/pages/toggle.js
+++ src/js/pages/toggle.js
@@ -4,4 +4,9 @@ $(function () {
   $(".jsc-toggle-button-main").on("click", function () {
     $(".jsc-toggle-button-main,.jsc-nav-wrap-main").toggleClass("is-open");
   });
+  
+   $(".jsc-nav-close").on("click", function () {
+    $(".jsc-toggle-button-main,.jsc-nav-wrap-main").removeClass("is-open");
+  });
+
 });
